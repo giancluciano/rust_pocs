375e009e37dccdb4731c374caa4795d9b0861a4e
gian <giancluciano@gmail.com>
2025-12-17 12:17:43 -0500

    add progress bar

--- a_cli/src/main.rs
+++ a_cli/src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::io::{self, Write};
 
 #[derive(Parser)]
 struct Cli {
@@ -9,13 +10,21 @@ struct Cli {
 fn main() {
 
     let args = Cli::parse();
+    let stdout = io::stdout();
+    let mut handle = io::BufWriter::new(stdout);
 
-    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+    let _ = writeln!(handle, "pattern: {:?}, path: {:?}", args.pattern, args.path);
     let content = std::fs::read_to_string(&args.path).expect("could not read file");
 
+    let pb = indicatif::ProgressBar::new(100);
+    for i in 0..100 {
+        pb.println(format!("[+] finished #{}", i));
+        pb.inc(1);
+    }
+    pb.finish_with_message("done");
     for line in content.lines() {
         if line.contains(&args.pattern) {
-            println!("{}", line);
+            let _ = writeln!(handle, "{}", line);
         }
     }
 }
