67733797755424f491e87e328ae437876048f0d9
gian <giancluciano@gmail.com>
2026-01-08 13:58:53 -0300

    add poc

--- axum_basics/src/main.rs
+++ axum_basics/src/main.rs
@@ -1,21 +1,52 @@
-use axum::{
-    routing::get,
-    Router,
-};
+use axum::{response::{Response, IntoResponse}, Json, http::StatusCode};
+use serde_json::json;
+use serde::Serialize;
+use axum::{Router, routing::get};
+use axum::Json;
+use serde_json::Value;
 
-// function handlers for the router
-async fn root() {}
-async fn get_foo() {}
-async fn post_foo() {}
-async fn foo_bar() {}
+// here we show a type that implements Serialize + Send
+#[derive(Serialize)]
+struct Message {
+    message: String
+}
+
+enum ApiResponse {
+    OK,
+    Created,
+    JsonData(Vec<Message>),
+}
+
+enum ApiError {
+    BadRequest,
+    Forbidden,
+    Unauthorised,
+    InternalServerError
+}
+
+impl IntoResponse for ApiResponse {
+    fn into_response(self) -> Response {
+        match self {
+            Self::OK => (StatusCode::OK).into_response(),
+            Self::Created => (StatusCode::CREATED).into_response(),
+            Self::JsonData(data) => (StatusCode::OK, Json(data)).into_response()
+        }
+    }
+}
+
+async fn get_user() -> Result<ApiResponse, ApiError> {
+    // Return a dummy user for poc
+    let data = vec![
+        Message { message: "Dummy User".to_string() }
+    ];
+    Ok(ApiResponse::JsonData(data))
+}
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
     let app = Router::new()
-        .route("/", get(root))
-        .route("/foo", get(get_foo).post(post_foo))
-        .route("/foo/bar", get(foo_bar));
+        .route("/user", get(get_user));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
