95bf731a0725fd7cb9582d2107c7815fa7c67881
gian <giancluciano@gmail.com>
2026-01-19 19:16:09 -0300

    fix code

--- tax_system/src/main.rs
+++ tax_system/src/main.rs
@@ -5,6 +5,8 @@ use std::env;
 pub mod models;
 pub mod schema;
 
+use self::models::*;
+
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
 
@@ -13,6 +15,38 @@ pub fn establish_connection() -> SqliteConnection {
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
 
+pub fn create_product(conn: &mut SqliteConnection, product_name: &str, product_value: &i32) -> Product {
+    use crate::schema::products;
+
+    let new_product = NewProduct { product_name, product_value };
+
+    diesel::insert_into(products::table)
+        .values(&new_product)
+        .returning(Product::as_returning())
+        .get_result(conn)
+        .expect("Error saving new post")
+}
+
 fn main() {
-    println!("Hello, world!");
+    use self::schema::products::dsl::*;
+    
+    let connection = &mut establish_connection();
+
+
+    let name = "phone";
+    let value = 100;
+
+    let _ = create_product(connection, &name, &value);
+
+    let results = products
+        .limit(5)
+        .select(Product::as_select())
+        .load(connection)
+        .expect("Error loading products");
+
+    println!("Displaying {} products", results.len());
+    for product in results {
+        println!("{}", product.product_name);
+        println!("-----------\n");
+    }
 }
--- tax_system/src/models.rs
+++ tax_system/src/models.rs
@@ -3,16 +3,23 @@ use diesel::prelude::*;
 #[derive(Queryable, Selectable)]
 #[diesel(table_name = crate::schema::products)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
-pub struct Post {
+pub struct Product {
     pub id: i32,
     pub product_name: String,
     pub product_value: i32,
 }
 
+#[derive(Insertable)]
+#[diesel(table_name = crate::schema::products)]
+pub struct NewProduct<'a> {
+    pub product_name: &'a str,
+    pub product_value: &'a i32,
+}
+
 #[derive(Queryable, Selectable)]
 #[diesel(table_name = crate::schema::taxes)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
-pub struct Post {
+pub struct Tax {
     pub id: i32,
     pub state_name: String,
     pub year: i32,
