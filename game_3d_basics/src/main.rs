4dd15177fa78ed83956903ee39df1ed3778ddb3a
gian <giancluciano@gmail.com>
2026-02-17 13:10:45 -0300

    new poc

--- game_3d_basics/src/main.rs
+++ game_3d_basics/src/main.rs
@@ -1,233 +1,211 @@
-use std::f32::consts::{FRAC_PI_4, PI};
+use std::f32::consts::FRAC_PI_2;
 
 use bevy::{
-    camera_controller::free_camera::{FreeCamera, FreeCameraPlugin, FreeCameraState},
-    color::palettes::tailwind,
-    prelude::*,
+    camera::visibility::RenderLayers, color::palettes::tailwind,
+    input::mouse::AccumulatedMouseMotion, light::NotShadowCaster, prelude::*,
 };
 fn main() {
     App::new()
-    .add_plugins(DefaultPlugins)
-    .add_plugins(FreeCameraPlugin)
-    // Example code plugins
-    .add_plugins((CameraPlugin, CameraSettingsPlugin, ScenePlugin))
-    .run();
+        .add_plugins(DefaultPlugins)
+        .add_systems(
+            Startup,
+            (
+                spawn_view_model,
+                spawn_world_model,
+                spawn_lights,
+                spawn_text,
+            ),
+        )
+        .add_systems(Update, (move_player, change_fov))
+        .run();
 }
+#[derive(Debug, Component)]
+struct Player;
 
-struct CameraPlugin;
-impl Plugin for CameraPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_camera);
+#[derive(Debug, Component, Deref, DerefMut)]
+struct CameraSensitivity(Vec2);
+
+impl Default for CameraSensitivity {
+    fn default() -> Self {
+        Self(
+            // These factors are just arbitrary mouse sensitivity values.
+            // It's often nicer to have a faster horizontal sensitivity than vertical.
+            // We use a component for them so that we can make them user-configurable at runtime
+            // for accessibility reasons.
+            // It also allows you to inspect them in an editor if you `Reflect` the component.
+            Vec2::new(0.003, 0.002),
+        )
     }
 }
 
-fn spawn_camera(mut commands: Commands) {
+#[derive(Debug, Component)]
+struct WorldModelCamera;
+
+/// Used implicitly by all entities without a `RenderLayers` component.
+/// Our world model camera and all objects other than the player are on this layer.
+/// The light source belongs to both layers.
+const DEFAULT_RENDER_LAYER: usize = 0;
+
+/// Used by the view model camera and the player's arm.
+/// The light source belongs to both layers.
+const VIEW_MODEL_RENDER_LAYER: usize = 1;
+
+fn spawn_view_model(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let arm = meshes.add(Cuboid::new(0.1, 0.1, 0.5));
+    let arm_material = materials.add(Color::from(tailwind::TEAL_200));
+
     commands.spawn((
-        Camera3d::default(),
-        Transform::from_xyz(0.0, 1.0, 0.0).looking_to(Vec3::X, Vec3::Y),
-        // This component stores all camera settings and state, which is used by the FreeCameraPlugin to
-        // control it. These properties can be changed at runtime, but beware the controller system is
-        // constantly using and modifying those values unless the enabled field is false.
-        FreeCamera {
-            sensitivity: 0.2,
-            friction: 25.0,
-            walk_speed: 3.0,
-            run_speed: 9.0,
-            ..default()
-        },
+        Player,
+        CameraSensitivity::default(),
+        Transform::from_xyz(0.0, 1.0, 0.0),
+        Visibility::default(),
+        children![
+            (
+                WorldModelCamera,
+                Camera3d::default(),
+                Projection::from(PerspectiveProjection {
+                    fov: 90.0_f32.to_radians(),
+                    ..default()
+                }),
+            ),
+            // Spawn view model camera.
+            (
+                Camera3d::default(),
+                Camera {
+                    // Bump the order to render on top of the world model.
+                    order: 1,
+                    ..default()
+                },
+                Projection::from(PerspectiveProjection {
+                    fov: 70.0_f32.to_radians(),
+                    ..default()
+                }),
+                // Only render objects belonging to the view model.
+                RenderLayers::layer(VIEW_MODEL_RENDER_LAYER),
+            ),
+            // Spawn the player's right arm.
+            (
+                Mesh3d(arm),
+                MeshMaterial3d(arm_material),
+                Transform::from_xyz(0.2, -0.1, -0.25),
+                // Ensure the arm is only rendered by the view model camera.
+                RenderLayers::layer(VIEW_MODEL_RENDER_LAYER),
+                // The arm is free-floating, so shadows would look weird.
+                NotShadowCaster,
+            ),
+        ],
     ));
 }
 
-// Plugin that handles camera settings controls and information text
-struct CameraSettingsPlugin;
-impl Plugin for CameraSettingsPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_systems(PostStartup, spawn_text)
-            .add_systems(Update, (update_camera_settings, update_text));
-    }
+fn spawn_world_model(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let floor = meshes.add(Plane3d::new(Vec3::Y, Vec2::splat(10.0)));
+    let cube = meshes.add(Cuboid::new(2.0, 0.5, 1.0));
+    let material = materials.add(Color::WHITE);
+
+    // The world model camera will render the floor and the cubes spawned in this system.
+    // Assigning no `RenderLayers` component defaults to layer 0.
+
+    commands.spawn((Mesh3d(floor), MeshMaterial3d(material.clone())));
+
+    commands.spawn((
+        Mesh3d(cube.clone()),
+        MeshMaterial3d(material.clone()),
+        Transform::from_xyz(0.0, 0.25, -3.0),
+    ));
+
+    commands.spawn((
+        Mesh3d(cube),
+        MeshMaterial3d(material),
+        Transform::from_xyz(0.75, 1.75, 0.0),
+    ));
 }
 
-#[derive(Component)]
-struct InfoText;
-
-fn spawn_text(mut commands: Commands, free_camera_query: Query<&FreeCamera>) {
+fn spawn_lights(mut commands: Commands) {
     commands.spawn((
-        Node {
-            position_type: PositionType::Absolute,
-            top: px(-16),
-            left: px(12),
+        PointLight {
+            color: Color::from(tailwind::ROSE_300),
+            shadows_enabled: true,
             ..default()
         },
-        children![Text::new(format!(
-            "{}",
-            free_camera_query.single().unwrap()
-        ))],
+        Transform::from_xyz(-2.0, 4.0, -0.75),
+        // The light source illuminates both the world model and the view model.
+        RenderLayers::from_layers(&[DEFAULT_RENDER_LAYER, VIEW_MODEL_RENDER_LAYER]),
     ));
-    commands.spawn((
-        Node {
+}
+
+fn spawn_text(mut commands: Commands) {
+    commands
+        .spawn(Node {
             position_type: PositionType::Absolute,
             bottom: px(12),
             left: px(12),
             ..default()
-        },
-        children![Text::new(concat![
-            "Z/X: decrease/increase sensitivity\n",
-            "C/V: decrease/increase friction\n",
-            "F/G: decrease/increase scroll factor\n",
-            "B: enable/disable controller",
-        ]),],
-    ));
-
-    // Mutable text marked with component
-    commands.spawn((
-        Node {
-            position_type: PositionType::Absolute,
-            top: px(12),
-            right: px(12),
-            ..default()
-        },
-        children![(InfoText, Text::new(""))],
-    ));
+        })
+        .with_child(Text::new(concat!(
+            "Move the camera with your mouse.\n",
+            "Press arrow up to decrease the FOV of the world model.\n",
+            "Press arrow down to increase the FOV of the world model."
+        )));
 }
 
-fn update_camera_settings(
-    mut camera_query: Query<(&mut FreeCamera, &mut FreeCameraState)>,
+fn move_player(
+    accumulated_mouse_motion: Res<AccumulatedMouseMotion>,
+    player: Single<(&mut Transform, &CameraSensitivity), With<Player>>,
+) {
+    let (mut transform, camera_sensitivity) = player.into_inner();
+
+    let delta = accumulated_mouse_motion.delta;
+
+    if delta != Vec2::ZERO {
+        // Note that we are not multiplying by delta_time here.
+        // The reason is that for mouse movement, we already get the full movement that happened since the last frame.
+        // This means that if we multiply by delta_time, we will get a smaller rotation than intended by the user.
+        // This situation is reversed when reading e.g. analog input from a gamepad however, where the same rules
+        // as for keyboard input apply. Such an input should be multiplied by delta_time to get the intended rotation
+        // independent of the framerate.
+        let delta_yaw = -delta.x * camera_sensitivity.x;
+        let delta_pitch = -delta.y * camera_sensitivity.y;
+
+        let (yaw, pitch, roll) = transform.rotation.to_euler(EulerRot::YXZ);
+        let yaw = yaw + delta_yaw;
+
+        // If the pitch was ±¹⁄₂ π, the camera would look straight up or down.
+        // When the user wants to move the camera back to the horizon, which way should the camera face?
+        // The camera has no way of knowing what direction was "forward" before landing in that extreme position,
+        // so the direction picked will for all intents and purposes be arbitrary.
+        // Another issue is that for mathematical reasons, the yaw will effectively be flipped when the pitch is at the extremes.
+        // To not run into these issues, we clamp the pitch to a safe range.
+        const PITCH_LIMIT: f32 = FRAC_PI_2 - 0.01;
+        let pitch = (pitch + delta_pitch).clamp(-PITCH_LIMIT, PITCH_LIMIT);
+
+        transform.rotation = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
+    }
+}
+
+fn change_fov(
     input: Res<ButtonInput<KeyCode>>,
+    mut world_model_projection: Single<&mut Projection, With<WorldModelCamera>>,
 ) {
-    let (mut free_camera, mut free_camera_state) = camera_query.single_mut().unwrap();
+    let Projection::Perspective(perspective) = world_model_projection.as_mut() else {
+        unreachable!(
+            "The `Projection` component was explicitly built with `Projection::Perspective`"
+        );
+    };
 
-    if input.pressed(KeyCode::KeyZ) {
-        free_camera.sensitivity = (free_camera.sensitivity - 0.005).max(0.005);
+    if input.pressed(KeyCode::ArrowUp) {
+        perspective.fov -= 1.0_f32.to_radians();
+        perspective.fov = perspective.fov.max(20.0_f32.to_radians());
     }
-    if input.pressed(KeyCode::KeyX) {
-        free_camera.sensitivity += 0.005;
+    if input.pressed(KeyCode::ArrowDown) {
+        perspective.fov += 1.0_f32.to_radians();
+        perspective.fov = perspective.fov.min(160.0_f32.to_radians());
     }
-    if input.pressed(KeyCode::KeyC) {
-        free_camera.friction = (free_camera.friction - 0.2).max(0.0);
-    }
-    if input.pressed(KeyCode::KeyV) {
-        free_camera.friction += 0.2;
-    }
-    if input.pressed(KeyCode::KeyF) {
-        free_camera.scroll_factor = (free_camera.scroll_factor - 0.02).max(0.02);
-    }
-    if input.pressed(KeyCode::KeyG) {
-        free_camera.scroll_factor += 0.02;
-    }
-    if input.just_pressed(KeyCode::KeyB) {
-        free_camera_state.enabled = !free_camera_state.enabled;
-    }
-}
-
-fn update_text(
-    mut text_query: Query<&mut Text, With<InfoText>>,
-    camera_query: Query<(&FreeCamera, &FreeCameraState)>,
-) {
-    let mut text = text_query.single_mut().unwrap();
-
-    let (free_camera, free_camera_state) = camera_query.single().unwrap();
-
-    text.0 = format!(
-        "Enabled: {},\nSensitivity: {:.03}\nFriction: {:.01}\nScroll factor: {:.02}\nWalk Speed: {:.02}\nRun Speed: {:.02}\nSpeed: {:.02}",
-        free_camera_state.enabled,
-        free_camera.sensitivity,
-        free_camera.friction,
-        free_camera.scroll_factor,
-        free_camera.walk_speed,
-        free_camera.run_speed,
-        free_camera_state.velocity.length(),
-    );
-}
-
-// Plugin that spawns the scene and lighting.
-struct ScenePlugin;
-impl Plugin for ScenePlugin {
-    fn build(&self, app: &mut App) {
-        app.add_systems(Startup, (spawn_lights, spawn_world));
-    }
-}
-
-fn spawn_lights(mut commands: Commands) {
-    // Main light
-    commands.spawn((
-        PointLight {
-            color: Color::from(tailwind::ORANGE_300),
-            shadows_enabled: false,
-            ..default()
-        },
-        Transform::from_xyz(0.0, 3.0, 0.0),
-    ));
-    // Light behind wall
-    commands.spawn((
-        PointLight {
-            color: Color::WHITE,
-            shadows_enabled: false,
-            ..default()
-        },
-        Transform::from_xyz(-3.5, 3.0, 0.0),
-    ));
-    // Light under floor
-    commands.spawn((
-        PointLight {
-            color: Color::from(tailwind::RED_300),
-            shadows_enabled: false,
-            ..default()
-        },
-        Transform::from_xyz(0.0, -0.5, 0.0),
-    ));
-}
-
-fn spawn_world(
-    mut commands: Commands,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    mut meshes: ResMut<Assets<Mesh>>,
-) {
-    let cube = meshes.add(Cuboid::new(1.0, 1.0, 1.0));
-    let floor = meshes.add(Plane3d::new(Vec3::Y, Vec2::splat(10.0)));
-    let sphere = meshes.add(Sphere::new(0.5));
-    let wall = meshes.add(Cuboid::new(0.2, 4.0, 3.0));
-
-    let blue_material = materials.add(Color::from(tailwind::BLUE_700));
-    let red_material = materials.add(Color::from(tailwind::RED_950));
-    let white_material = materials.add(Color::WHITE);
-
-    // Top side of floor
-    commands.spawn((
-        Mesh3d(floor.clone()),
-        MeshMaterial3d(white_material.clone()),
-    ));
-    // Under side of floor
-    commands.spawn((
-        Mesh3d(floor.clone()),
-        MeshMaterial3d(white_material.clone()),
-        Transform::from_xyz(0.0, -0.01, 0.0).with_rotation(Quat::from_rotation_x(PI)),
-    ));
-    // Blue sphere
-    commands.spawn((
-        Mesh3d(sphere.clone()),
-        MeshMaterial3d(blue_material.clone()),
-        Transform::from_xyz(3.0, 1.5, 0.0),
-    ));
-    // Tall wall
-    commands.spawn((
-        Mesh3d(wall.clone()),
-        MeshMaterial3d(white_material.clone()),
-        Transform::from_xyz(-3.0, 2.0, 0.0),
-    ));
-    // Cube behind wall
-    commands.spawn((
-        Mesh3d(cube.clone()),
-        MeshMaterial3d(blue_material.clone()),
-        Transform::from_xyz(-4.2, 0.5, 0.0),
-    ));
-    // Hidden cube under floor
-    commands.spawn((
-        Mesh3d(cube.clone()),
-        MeshMaterial3d(red_material.clone()),
-        Transform {
-            translation: Vec3::new(3.0, -2.0, 0.0),
-            rotation: Quat::from_euler(EulerRot::YXZEx, FRAC_PI_4, FRAC_PI_4, 0.0),
-            ..default()
-        },
-    ));
 }
